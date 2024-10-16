8b4901c105fed32b72e57c4a1ec903b669c4814f
mohsin <87213341+mohsinkhan25@users.noreply.github.com>
2024-10-15 23:15:51 -0500

    Updated Radix sort implementation that works

--- MPI_Builds/radixsort.cpp
+++ MPI_Builds/radixsort.cpp
@@ -1,9 +1,9 @@
 #include "mpi.h"
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <cmath>
 #include <string>
+#include <vector>
 #include <caliper/cali.h>
 #include <caliper/cali-manager.h>
 #include <adiak.hpp>
@@ -24,12 +24,12 @@ void counting_sort(int* arr, int size, int exp) {
     int output[size]; 
     int count[BASE] = {0};
 
-    // Count occurrences of digits
+    // Count occurrences of digits based on current exponent
     for (int i = 0; i < size; i++) {
         count[(arr[i] / exp) % BASE]++;
     }
 
-    // Update count[] to contain positions of digits in the output array
+    // Transform count array to represent positions in the output array
     for (int i = 1; i < BASE; i++) {
         count[i] += count[i - 1];
     }
@@ -73,24 +73,29 @@ void generate_data(int* data, int N, const std::string& input_type) {
 }
 
 bool check_sorted_correctly(int* local_data, int local_size, int rank, int size, MPI_Comm comm) {
-    for (int i = 1; i < local_size; i++) {
-        if (local_data[i - 1] > local_data[i]) {
-            return false;
-        }
+    int local_last = local_data[local_size - 1];
+    int* boundary_values = nullptr;
+
+    if (rank == 0) {
+        boundary_values = new int[size];
     }
 
-    // Check that the last value of this process is <= first value of the next process
-    if (rank != size - 1) {
-        int local_last = local_data[local_size - 1];
-        int next_first;
-        MPI_Send(&local_last, 1, MPI_INT, rank + 1, 0, comm);
-        MPI_Recv(&next_first, 1, MPI_INT, rank + 1, 0, comm, MPI_STATUS_IGNORE);
-        if (local_last > next_first) {
-            return false;
+    MPI_Gather(&local_last, 1, MPI_INT, boundary_values, 1, MPI_INT, 0, comm);
+
+    bool global_sorted = true;
+    if (rank == 0) {
+        for (int i = 1; i < size; i++) {
+            if (boundary_values[i - 1] > boundary_values[i]) {
+                global_sorted = false;
+                break;
+            }
         }
+        delete[] boundary_values;
     }
 
-    return true;
+    MPI_Bcast(&global_sorted, 1, MPI_C_BOOL, 0, comm);
+
+    return global_sorted;
 }
 
 int main(int argc, char** argv) {
@@ -105,7 +110,7 @@ int main(int argc, char** argv) {
     MPI_Comm_rank(comm, &rank);
     MPI_Comm_size(comm, &size);
 
-    if (argc != 1) {
+    if (argc != 2) {
         if (rank == 0) {
             printf("Usage: %s <input_size_power_of_2> <input_type (random|sorted|reverse|perturbed)>\n", argv[0]);
         }
@@ -114,12 +119,8 @@ int main(int argc, char** argv) {
     }
 
     int N_power = atoi(argv[1]);      // Input size as power of 2 (e.g., 16 for 2^16)
-    // std::string input_type = argv[2]; // Input type (random, sorted, reverse, perturbed)
     std::string input_type = "random";
     int N = pow(2, N_power);          // Calculate 2^N
-
-    adiak::value("input_size", N);       
-    adiak::value("input_type", input_type.c_str()); 
 
     int fullArray[N];  
     int subArray[N / size];  
@@ -158,34 +159,70 @@ int main(int argc, char** argv) {
 
         CALI_MARK_BEGIN("comm");
         CALI_MARK_BEGIN("comm_large");
-        int received_data[N];  
-        MPI_Alltoall(subArray, N / size, MPI_INT, received_data, N / size, MPI_INT, comm);
-        for (int i = 0; i < (N / size) * size; i++) {
-            subArray[i] = received_data[i];
+
+        // Redistribute data using bucketing based on current digit
+        std::vector<int> buckets[size];
+        for (int i = 0; i < N / size; i++) {
+            int bucket_idx = (subArray[i] / exp) % size;
+            buckets[bucket_idx].push_back(subArray[i]);
         }
+
+        // Prepare for MPI_Alltoallv
+        int send_counts[size];
+        int recv_counts[size];
+        int send_displs[size];
+        int recv_displs[size];
+
+        // Calculate send_counts
+        for (int i = 0; i < size; i++) {
+            send_counts[i] = buckets[i].size();
+        }
+
+        MPI_Alltoall(send_counts, 1, MPI_INT, recv_counts, 1, MPI_INT, comm);
+
+        // Calculate displacements for MPI_Alltoallv
+        send_displs[0] = 0;
+        recv_displs[0] = 0;
+        for (int i = 1; i < size; i++) {
+            send_displs[i] = send_displs[i - 1] + send_counts[i - 1];
+            recv_displs[i] = recv_displs[i - 1] + recv_counts[i - 1];
+        }
+
+        // Flatten the buckets into send buffer
+        std::vector<int> send_buffer;
+        for (int i = 0; i < size; i++) {
+            send_buffer.insert(send_buffer.end(), buckets[i].begin(), buckets[i].end());
+        }
+
+        std::vector<int> recv_buffer(recv_displs[size - 1] + recv_counts[size - 1]);
+        MPI_Alltoallv(send_buffer.data(), send_counts, send_displs, MPI_INT,
+                      recv_buffer.data(), recv_counts, recv_displs, MPI_INT, comm);
+
+        // Replace the subArray with the received data
+        std::copy(recv_buffer.begin(), recv_buffer.end(), subArray);
+
         CALI_MARK_END("comm_large");
         CALI_MARK_END("comm");
 
         CALI_CXX_MARK_LOOP_END(sort_loop);
     }
 
+    CALI_MARK_BEGIN("comm");
+    CALI_MARK_BEGIN("comm_large");
+    MPI_Gather(subArray, N / size, MPI_INT, (rank == 0) ? fullArray : NULL, N / size, MPI_INT, 0, comm);
+    CALI_MARK_END("comm_large");
+    CALI_MARK_END("comm");
+
     if (rank == 0) {
-        CALI_MARK_BEGIN("comm");
-        CALI_MARK_BEGIN("comm_large");
-        MPI_Gather(subArray, N / size, MPI_INT, fullArray, N / size, MPI_INT, 0, comm);
-        CALI_MARK_END("comm_large");
-        CALI_MARK_END("comm");
         printf("Sorting completed.\n");
     }
 
     CALI_MARK_BEGIN("correctness_check");
     bool sorted_correctly = check_sorted_correctly(subArray, N / size, rank, size, comm);
-    bool global_sorted;
-    MPI_Reduce(&sorted_correctly, &global_sorted, 1, MPI_C_BOOL, MPI_LAND, 0, comm);
     CALI_MARK_END("correctness_check");
 
     if (rank == 0) {
-        if (global_sorted) {
+        if (sorted_correctly) {
             printf("Array is sorted correctly.\n");
         } else {
             printf("Array is NOT sorted correctly.\n");
@@ -193,24 +230,23 @@ int main(int argc, char** argv) {
     }
 
     adiak::init(NULL);
-    adiak::launchdate();    // launch date of the job
-    adiak::libraries();     // Libraries used
-    adiak::cmdline();       // Command line used to launch the job
-    adiak::clustername();   // Name of the cluster
-    adiak::value("algorithm", "radix"); // The name of the algorithm you are using (e.g., "merge", "bitonic")
-    adiak::value("programming_model", "mpi"); // e.g. "mpi"
-    adiak::value("data_type", "int"); // The datatype of input elements (e.g., double, int, float)
-    adiak::value("size_of_data_type", sizeof(int)); // sizeof(datatype) of input elements in bytes (e.g., 1, 2, 4)
-    adiak::value("input_size", N); // The number of elements in input dataset (1000)
-    adiak::value("input_type", input_type); // For sorting, this would be choices: ("Sorted", "ReverseSorted", "Random", "1_perc_perturbed")
-    adiak::value("num_procs", size); // The number of processors (MPI ranks)
-    adiak::value("scalability", "strong"); // The scalability of your algorithm. choices: ("strong", "weak")
-    adiak::value("group_num", "5"); // The number of your group (integer, e.g., 1, 10)
-    adiak::value("implementation_source", "online and ai"); // Where you got the source code of your algorithm. choices: ("online", "ai", "handwritten").
+    adiak::launchdate();
+    adiak::libraries();
+    adiak::cmdline();
+    adiak::clustername();
+    adiak::value("algorithm", "radix");
+    adiak::value("programming_model", "mpi");
+    adiak::value("data_type", "int");
+    adiak::value("size_of_data_type", sizeof(int));
+    adiak::value("input_size", N);
+    adiak::value("input_type", input_type.c_str());
+    adiak::value("num_procs", size);
+    adiak::value("scalability", "strong");
+    adiak::value("group_num", "5");
+    adiak::value("implementation_source", "online and ai");
 
     mgr.stop();
     mgr.flush();
-    adiak::finalize();
     MPI_Finalize();
 
     return 0;
